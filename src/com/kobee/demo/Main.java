ee30e6002b33c3d79eed8941afd5cc96fc0219f0
Kobee Build Agent <kobee@cyntexa.com>
2026-09-01 14:19:33 +0530

    IK0001040 "second commit"

---
+++ src/com/kobee/demo/Main.java
@@ -0,0 +1,7 @@
+package com.kobee.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MessageUtils.getMessage());
+    }
+}
---
+++ src/com/kobee/demo/MessageUtils.java
@@ -0,0 +1,7 @@
+package com.kobee.demo;
+
+public class MessageUtils {
+    public static String getMessage() {
+        return "Hello! This is a Java Package-Based Build from Kobee!";
+    }
+}
